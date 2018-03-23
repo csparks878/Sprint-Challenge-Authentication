910786d31b26e8662b24544ad2571804137da059
Charlie Sparks <csparks@antioch.edu>
2018-03-23 10:59:14 -0700

    pre-save hook and check password middleware built

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -27,6 +27,11 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  })
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -34,6 +39,10 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, matched) => {
+    if (err) return callBack(err);
+    if (!err && matched) callBack(null, matched);
+  })
 };
 
 // if you're really stuck with this at this point, you can reference this document.
